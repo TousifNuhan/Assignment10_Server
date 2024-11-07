9a6563ef80a3f6ff8259d03fd22c1f6a449b07e4
Gazi Mubtasim Tousif Nuhan <mubtasimtousif@gmail.com>
2024-11-07 22:22:33 +0600

    Done

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
-const app = express()
+require('dotenv').config();
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
+
 app.use(express.json())
 
 
@@ -23,34 +24,45 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        const tourismCollection=client.db("TourismDb").collection("TouristCollection")
-        const userCollection=client.db("TourismDb").collection("user")
+        const tourismCollection = client.db("TourismDb").collection("TouristCollection")
+        const userCollection = client.db("TourismDb").collection("user")
 
-        app.get('/spot',async(req,res)=>{
-            const cursor=tourismCollection.find()
-            const result=await cursor.toArray()
-            res.send(result)
-        })
+        // app.get('/spot',async(req,res)=>{
+        //     const cursor=tourismCollection.find()
+        //     const result=await cursor.toArray()
+        //     res.send(result)
+        // })
 
-        app.post('/spot', async(req,res)=>{
-            const spot=req.body
-            const result=await tourismCollection.insertOne(spot)
+        app.get('/spot', async (req, res) => {
+            try {
+                const cursor = tourismCollection.find();
+                const result = await cursor.toArray();
+                res.send(result);
+            } catch (error) {
+                console.error('Error fetching spots:', error);
+                res.status(500).send('Error fetching spots');
+            }
+        });
+
+        app.post('/spot', async (req, res) => {
+            const spot = req.body
+            const result = await tourismCollection.insertOne(spot)
             res.send(result)
         })
 
         //user
 
-        app.post('/user', async(req,res)=>{
-            const user=req.body
+        app.post('/user', async (req, res) => {
+            const user = req.body
             console.log(user)
-            const result=await userCollection.insertOne(user)
+            const result = await userCollection.insertOne(user)
             console.log(result)
             res.send(result)
         })
 
         //lookup
-        app.get('/user',async(req,res)=>{
-            
+        app.get('/user', async (req, res) => {
+
             const result = await userCollection.aggregate([
                 {
                     $lookup: {
@@ -60,59 +72,59 @@ async function run() {
                         as: "touristSpots"
                     }
                 },
-               
+
             ]).toArray();
             console.log(result)
             res.send(result)
         })
 
-        app.get('/user/:id',async(req,res)=>{
-            const id=req.params.id
+        app.get('/user/:id', async (req, res) => {
+            const id = req.params.id
             console.log(id)
-            const query={_id: new ObjectId(id)}
-            const result=await tourismCollection.findOne(query)
+            const query = { _id: new ObjectId(id) }
+            const result = await tourismCollection.findOne(query)
             res.send(result)
         })
 
-        app.put('/user/:id',async(req,res)=>{
-            const id=req.params.id
-            const updatedUserInfo=req.body
-            const filter={_id : new ObjectId(id)}
-            const options={upsert:true}
-            const userInfo={
-                $set:{
-                    userName:updatedUserInfo.userName,
-                     email:updatedUserInfo.email,
-                     countryName:updatedUserInfo.countryName,
-                     spotName:updatedUserInfo.spotName,
-                     location:updatedUserInfo.location,
-                     photoURL:updatedUserInfo.photoURL,
-                     averageCost:updatedUserInfo.averageCost,
-                     seasonality:updatedUserInfo.seasonality,
-                     travelTime:updatedUserInfo.travelTime,
-                     totalVisitors:updatedUserInfo.totalVisitors,
-                     description:updatedUserInfo.description
+        app.put('/user/:id', async (req, res) => {
+            const id = req.params.id
+            const updatedUserInfo = req.body
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const userInfo = {
+                $set: {
+                    userName: updatedUserInfo.userName,
+                    email: updatedUserInfo.email,
+                    countryName: updatedUserInfo.countryName,
+                    spotName: updatedUserInfo.spotName,
+                    location: updatedUserInfo.location,
+                    photoURL: updatedUserInfo.photoURL,
+                    averageCost: updatedUserInfo.averageCost,
+                    seasonality: updatedUserInfo.seasonality,
+                    travelTime: updatedUserInfo.travelTime,
+                    totalVisitors: updatedUserInfo.totalVisitors,
+                    description: updatedUserInfo.description
 
                 }
             }
-            const result=await tourismCollection.updateOne(filter,userInfo,options)
+            const result = await tourismCollection.updateOne(filter, userInfo, options)
             // console.log(result.modifiedCount)
-            if(result.modifiedCount === 1){
-                const updateInfo=await userCollection.updateMany(
-                    {"touristSpots._id": new ObjectId(id)},
+            if (result.modifiedCount === 1) {
+                const updateInfo = await userCollection.updateMany(
+                    { "touristSpots._id": new ObjectId(id) },
                     {
-                        $set:{
-                            "touristSpots.$.userName":updatedUserInfo.userName,
-                            "touristSpots.$.email":updatedUserInfo.email,
-                            "touristSpots.$.countryName":updatedUserInfo.countryName,
-                            "touristSpots.$.spotName":updatedUserInfo.spotName,
-                            "touristSpots.$.location":updatedUserInfo.location,
-                            "touristSpots.$.photoURL":updatedUserInfo.photoURL,
-                            "touristSpots.$.averageCost":updatedUserInfo.averageCost,
-                            "touristSpots.$.seasonality":updatedUserInfo.seasonality,
-                            "touristSpots.$.travelTime":updatedUserInfo.travelTime,
-                            "touristSpots.$.totalVisitors":updatedUserInfo.totalVisitors,
-                            "touristSpots.$.description":updatedUserInfo.description,
+                        $set: {
+                            "touristSpots.$.userName": updatedUserInfo.userName,
+                            "touristSpots.$.email": updatedUserInfo.email,
+                            "touristSpots.$.countryName": updatedUserInfo.countryName,
+                            "touristSpots.$.spotName": updatedUserInfo.spotName,
+                            "touristSpots.$.location": updatedUserInfo.location,
+                            "touristSpots.$.photoURL": updatedUserInfo.photoURL,
+                            "touristSpots.$.averageCost": updatedUserInfo.averageCost,
+                            "touristSpots.$.seasonality": updatedUserInfo.seasonality,
+                            "touristSpots.$.travelTime": updatedUserInfo.travelTime,
+                            "touristSpots.$.totalVisitors": updatedUserInfo.totalVisitors,
+                            "touristSpots.$.description": updatedUserInfo.description,
                         }
                     }
                 )
@@ -122,22 +134,22 @@ async function run() {
             // res.send(result)
         })
 
-        app.delete('/user/:id',async(req,res)=>{
-            const id=req.params.id
-            console.log( 'deleted',id)
-            const query={_id: new ObjectId(id)}
-            const result=await tourismCollection.deleteOne(query)
-            if(result.deletedCount === 1){
-                const updateResult=await userCollection.updateMany(
-                    {"touristSpots._id": new ObjectId(id)},
-                    {$pull: { touristSpots: { _id: new ObjectId(id) } }}
-            )
-            console.log(updateResult)
-            res.send(updateResult)
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id
+            console.log('deleted', id)
+            const query = { _id: new ObjectId(id) }
+            const result = await tourismCollection.deleteOne(query)
+            if (result.deletedCount === 1) {
+                const updateResult = await userCollection.updateMany(
+                    { "touristSpots._id": new ObjectId(id) },
+                    { $pull: { touristSpots: { _id: new ObjectId(id) } } }
+                )
+                console.log(updateResult)
+                res.send(updateResult)
             }
         })
-        
-        
+
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
